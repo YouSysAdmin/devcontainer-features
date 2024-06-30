b759bfe50a8e25fdec36f4096ae2ea8f0ab0d75b
Oleksii Samoliyk <root@sysalex.com>
2024-06-30 20:54:40 +0300

    fix: neovim-configs install script

--- src/neovim-config/install.sh
+++ src/neovim-config/install.sh
@@ -5,7 +5,7 @@ USERNAME=${USERNAME:-"dev"}
 apt-get update -y
 apt-get -y install --no-install-recommends neovim
 
-su -l "${USERNAME}" -c "mkdir -p ${HOME}/.config"
-su -l "${USERNAME}" -c "cp -r $PWD/nvim ${HOME}/.config/nvim"
+su -l "${USERNAME}" -c "mkdir -p /home/${USERNAME}/.config"
+su -l "${USERNAME}" -c "cp -r $PWD/nvim /home/${USERNAME}/.config/nvim"
 
 rm -rf /var/lib/apt/lists/*
